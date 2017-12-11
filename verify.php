1948b0d0552c4cecd6988af7b4c8da58420cd4e1
BBOTPCCST <31894046+BBOTPCCST@users.noreply.github.com>
2017-12-11 21:05:33 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
+$access_token = 'crHidWVeiiN8uj7SAQRyWQckiB/neqPAcDL4ug41Y+5dviaOOKbnqElh3YwuGRmbqW/rOTggUXplHjr81FqBccnEW3K2XI/0FEdQEkhc8SmZrCVDl6gNb06lcK75QdbB7kvL2m27SfAKPaP0IwonzwdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
