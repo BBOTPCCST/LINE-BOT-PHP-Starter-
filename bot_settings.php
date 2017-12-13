97580f8e709ca1a39274aed84f0370b1ab25d8d0
BBOTPCCST <31894046+BBOTPCCST@users.noreply.github.com>
2017-12-13 19:59:34 +0700

    Update bot_settings.php

--- bot_settings.php
+++ bot_settings.php
@@ -1,6 +1,7 @@
 <?php
 /// การตั้งค่าเกี่ยวกับ bot ใน LINE Messaging API
-define('LINE_MESSAGE_CHANNEL_ID','กรอก ค่า Channel ID');
-define('LINE_MESSAGE_CHANNEL_SECRET','กรอกค่า Channel secret');
-define('LINE_MESSAGE_ACCESS_TOKEN','กรอกค่า Channel access token');
+define('LINE_MESSAGE_CHANNEL_ID','1550684737');
+define('LINE_MESSAGE_CHANNEL_SECRET','205aa6d3f2819ada8895a95186adaebd
+');
+define('LINE_MESSAGE_ACCESS_TOKEN','jJ1OerFIEnqAkCfDHIKrmFpVlQVijzWODJ67WlhzyaFLOJWq6tbIeCAAYyTlc7EWqW/rOTggUXplHjr81FqBccnEW3K2XI/0FEdQEkhc8SlMUz2Qjpa6jFyA48tbStwoexooEzi6c2HYgMvmFkg5MwdB04t89/1O/w1cDnyilFU=');
 ?>
